b0032bf780d24f37fef088782ddd2e1145048a94
HimAYouN <himayoun7931@gmail.com>
2026-02-08 10:52:37 +0530

    taxCalculator DONE ig

--- src/08-tax-calculator.js
+++ src/08-tax-calculator.js
@@ -27,6 +27,23 @@
  */
 export function calculateTax(income) {
   // Your code here
+  var taxable = 0
+  var tax = 0
   if(income<=0){return 0 }
-  else if(income>0 && income<=10000)return (income*10)/100
+  else if(income>0 && income<=10000)return 0
+  else if(income>10000 && income <=30000) {
+    taxable = income - 10000
+    tax = (taxable*10)/100
+    return  tax
+  }
+  else if(income>30000 && income <=70000) {
+    taxable = income - 30000
+    tax = (taxable*20)/100
+    return  2000 + tax
+  }
+  else if(income>70000) {
+    taxable = income - 70000
+    tax = (taxable*30)/100
+    return  10000+ tax
+  }
 }
